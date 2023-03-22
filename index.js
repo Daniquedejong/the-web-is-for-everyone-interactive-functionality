0678192a405131e1fa1bb3ebdf0f712ab41ad56b
Daniquedejong <danique.de.jong3@hva.nl>
2023-03-22 13:34:33 +0100

    v2

--- index.js
+++ index.js
@@ -36,3 +36,26 @@ async function fetchJson(url, payload = {}) {
     .then((response) => response.json())
     .catch((error) => error);
 }
+
+
+
+server.post('/new', (request, response) => {
+    const baseurl = "https://whois.fdnd.nl/api/v1"
+  
+    const url = `${baseurl}/member`
+  
+    postJson(url, request.body).then((data) => {
+      let newMember = { ... request.body }
+  
+      if (data.success) {
+        response.redirect('/?memberPosted=true') 
+        // TODO: squad meegeven, message meegeven
+        // TODO: Toast meegeven aan de homepagina
+      } else {
+        const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
+        const newdata = { error: errormessage, values: newMember }
+        
+        response.render('form', newdata)
+      }
+    })
+  })
