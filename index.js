8035af3bf7c74425dc97d647d6e10e9a76c989f3
Daniquedejong <danique.de.jong3@hva.nl>
2023-03-29 13:17:03 +0200

    v4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Importeer express uit de node_modules map
 import express from "express";
 
-// Maak een nieuwe express app aan
 const url = "https://api.vinimini.fdnd.nl/api/v1/producten"; // URL naar Json data
+const urlPost = "https://api.vinimini.fdnd.nl/api/v1/notities"; // Post URL 
 
+// Maak een nieuwe express app aan
 const app = express();
 
 // Stel ejs in als template engine en geef de 'views' map door
@@ -13,6 +14,10 @@ app.set("views", "./views");
 // Gebruik de map 'public' voor statische resources
 app.use(express.static("public"));
 
+// afhandeling van formulieren
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // Maak een route voor de index
 app.get("/", (request, response) => {
   let productenUrl = url;
@@ -20,6 +25,39 @@ app.get("/", (request, response) => {
   fetchJson(productenUrl).then((data) => {
     response.render("index", data);
   });
+});
+
+app.get("/hallo", (request, response) => {
+    response.render("logboek");
+});
+
+
+
+
+// ..............Post note to API.................
+//"/new" is de (post) actie van het formulier
+app.post('/newnote', function (req, res) {
+
+  req.body.afgerond = false
+  req.body.persoonId = 'clemozv3c3eod0bunahh71sx7'
+  req.body.datum = req.body.datum + ':00Z'
+  req.body.herinnering = [req.body.herinnering + ':00Z']
+  console.log("request body:")
+  console.log(req.body)
+
+  postJson(urlPost, req.body).then((data) => {
+
+    
+    if (data.success) {
+      console.log("POST gelukt")
+      res.redirect('/') 
+      // TODO: squad meegeven, message meegeven
+      // TODO: Toast meegeven aan de homepagina
+    } else {
+      
+      console.log("Post error", data.message)
+    }
+  })
 });
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -35,6 +73,16 @@ async function fetchJson(url, payload = {}) {
   return await fetch(url, payload)
     .then((response) => response.json())
     .catch((error) => error);
+}
+
+export async function postJson(url, body) {
+  return await fetch(url, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .catch((error) => error)
 }
 
 
